0371b4ad4ca0f39a903500dda786115382561afa
Arthur <quiquearthur5@gmail.com>
2019-06-04 18:47:24 +0200

    horizontal

--- PPProject/PPProject.Android/MainActivity.cs
+++ PPProject/PPProject.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace PPProject.Droid
 {
-    [Activity(Label = "Future Shock", Icon = "@mipmap/fsicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Future Shock", Icon = "@mipmap/fsicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

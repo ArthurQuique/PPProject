5116f563b00bbe0e1d806c98bfceff0e9f802a61
Faucheur faucheur <Faucheur-59@hotmail.fr>
2019-06-08 19:15:35 +0200

    layout et animation HomePage + layout MainPage + redirection

--- PPProject/PPProject/Pages/HomePage.xaml.cs
+++ PPProject/PPProject/Pages/HomePage.xaml.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace PPProject
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+
+		public HomePage ()
+		{
             InitializeComponent();
+            fadeTo();
         }
 
-        private void GamePage_Button_Clicked(object sender, EventArgs e)
+        private async void fadeTo()
         {
-            Navigation.PushAsync(new GamePage());
+            logo.Opacity = 0;
+            await logo.FadeTo(1, 4500);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new MainPage());
         }
+
     }
+
+
 }

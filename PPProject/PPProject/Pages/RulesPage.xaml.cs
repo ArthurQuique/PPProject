e9aa430f18275329dd779d5fa3cf65dceb997ea8
Faucheur faucheur <Faucheur-59@hotmail.fr>
2019-06-09 19:22:22 +0200

    menu up + création de toutes les pages

---
+++ PPProject/PPProject/Pages/RulesPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PPProject
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RulesPage : ContentPage
+	{
+		public RulesPage()
+		{
+			InitializeComponent();
+   
+		}
+
+       // String fileContent;
+
+       /* private void openRules()
+            {
+            using (StreamReader sr = File.OpenText("regles.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    fileContent = sr.ReadToEnd();
+                }
+            }
+        }*/
+	}
+}
